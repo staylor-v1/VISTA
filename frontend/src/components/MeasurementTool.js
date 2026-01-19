123a236a569885a4bd9624b69535ddd4d54589e3
Travis Dockter <travisdock@gmail.com>
2026-01-19 09:39:09 -0700

    Fix measurement save dialog positioning and add test coverage

--- frontend/src/components/MeasurementTool.js
+++ frontend/src/components/MeasurementTool.js
@@ -195,7 +195,7 @@ export default function MeasurementTool({
   if (!calibration) {
     return (
       <div style={{
-        position: 'absolute',
+        position: 'fixed',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
