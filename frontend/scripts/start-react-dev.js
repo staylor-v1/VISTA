4535ee0816c573500cd70e276744a3f05499555b
staylor-v1 <setaylo@sandia.gov>
2026-04-28 13:37:35 -0600

    Replace cross-env dev scripts with Node launcher

---
+++ frontend/scripts/start-react-dev.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+
+const isFastMode = process.argv.includes('--fast');
+const isCheckMode = process.argv.includes('--check');
+
+const env = {
+  ...process.env,
+  FAST_REFRESH: 'true',
+  SKIP_PREFLIGHT_CHECK: 'true'
+};
+
+if (isFastMode) {
+  env.GENERATE_SOURCEMAP = 'false';
+}
+
+if (isCheckMode) {
+  const checkSummary = {
+    FAST_REFRESH: env.FAST_REFRESH,
+    SKIP_PREFLIGHT_CHECK: env.SKIP_PREFLIGHT_CHECK,
+    GENERATE_SOURCEMAP: env.GENERATE_SOURCEMAP ?? '(default)'
+  };
+
+  console.log(JSON.stringify(checkSummary, null, 2));
+  process.exit(0);
+}
+
+const child = spawn('react-scripts', ['start'], {
+  env,
+  shell: true,
+  stdio: 'inherit'
+});
+
+child.on('exit', (code, signal) => {
+  if (signal) {
+    process.kill(process.pid, signal);
+    return;
+  }
+
+  process.exit(code ?? 0);
+});
