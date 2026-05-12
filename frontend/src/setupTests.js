cd46ad6bb7712843026fc0e362ad7e89dc7bd237
staylor-v1 <setaylo@sandia.gov>
2026-05-12 13:09:34 -0600

    Fix frontend ESLint warnings in app sources

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -109,8 +109,8 @@ if (typeof global.crypto === 'undefined') {
 if (typeof global.crypto.randomUUID === 'undefined') {
   global.crypto.randomUUID = () => {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-      const r = Math.random() * 16 | 0;
-      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      const r = Math.floor(Math.random() * 16);
+      const v = c === 'x' ? r : ((r & 0x3) | 0x8);
       return v.toString(16);
     });
   };
